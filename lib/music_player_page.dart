fd6c31aec19db2bf819f0e45c940e64aef9a9377
momin-mostafa <mominmostafa.g@gmail.com>
2022-11-16 16:04:41 +0600

    init and done

---
+++ lib/music_player_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MusicPlayerPage extends StatefulWidget {
+  const MusicPlayerPage({super.key});
+
+  @override
+  State<MusicPlayerPage> createState() => _MusicPlayerPageState();
+}
+
+class _MusicPlayerPageState extends State<MusicPlayerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
