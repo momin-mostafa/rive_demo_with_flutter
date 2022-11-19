6d7509fdd27ac2826a8e64bf2cad32b5c7a4e11b
momin-mostafa <mominmostafa.g@gmail.com>
2022-11-19 11:37:27 +0600

    State Machine Demo Done

---
+++ lib/only_touch_screen.dart
@@ -0,0 +1,72 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class TapAnimationScreen extends StatefulWidget {
+  const TapAnimationScreen({
+    super.key,
+  });
+
+  @override
+  State<TapAnimationScreen> createState() => _TapAnimationScreenState();
+}
+
+class _TapAnimationScreenState extends State<TapAnimationScreen> {
+  String riveFilePath = 'asset/star_animation.riv';
+  late RiveAnimationController controller;
+  Color color = Colors.grey;
+
+  void _changeAnimation(RiveAnimationController controller) {
+    if (controller.isActive == false) {
+      controller.isActive = true;
+      Future.delayed(const Duration(seconds: 1)).then((value) {
+        setState(() {
+          color = Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
+              .withOpacity(1.0);
+        });
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = OneShotAnimation(
+      'Scale',
+      autoplay: false,
+    );
+    // rootBundle.load(riveFilePath).then((data) {
+    //   final file = RiveFile.import(data);
+    //   final artboard = file.mainArtboard;
+    //   final stateMachineController =
+    //       StateMachineController.fromArtboard(artboard, 'StarAnimation');
+    //   if (stateMachineController != null) {
+    //     artboard.addController(stateMachineController);
+    //     _buttonInput = stateMachineController.findInput('Switch');
+    //     // _trigger = stateMachineController.findInput<bool>('Tap') as SMITrigger;
+    //     setState(
+    //       () => _artboard = artboard,
+    //     );
+    //   }
+    // });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: () => _changeAnimation(controller),
+          child: Container(
+            color: color,
+            child: RiveAnimation.asset(
+              riveFilePath,
+              controllers: [controller],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
